f9baa3a343486c33c2223fc3248e8d1027f6f5ce
iKasty <ikasty@live.co.kr>
2014-11-17 16:41:16 +0900

    menu load 관련 bug 수정

--- js/menu.js
+++ js/menu.js
@@ -37,7 +37,7 @@ menu_init = function() {
         overlay.className = 'bt-overlay';
         menu.appendChild( overlay );
  
-        if (typeof trigger === 'undefined') return ;
+        if (trigger === null) return ;
         trigger.addEventListener( eventtype, function( ev ) {
             ev.stopPropagation();
             ev.preventDefault();
