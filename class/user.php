a40603d5e7065a4a020dff549cc5a9f0a4090b86
서준혁 <routine91@gmail.com>
2014-11-25 21:01:29 +0900

    평가자료 클래스 추가

--- class/user.php
+++ class/user.php
@@ -18,7 +18,7 @@ class User
 	public $company = "";
 	public $department_id = "";
 
-	public $file_list[] = "";
+	public $file_list = array();
 
 	protected function __construct($user_id)
 	{
@@ -166,7 +166,7 @@ class User
 
 		$DB->Update('개발자',
 		array('developer_id'=>$_POST["id"],
-			'university'=>md5($_POST["대학교"]),
+			'university'=>$_POST["대학교"],
 			'hometown'=>$_POST["고향"]),
 		array("%s", "%s", "%s"),
 		array('id'=>$this->id),
@@ -245,7 +245,7 @@ class User
 		}
 
 	}
-
+	//평가라고 하기 부족함
 	function Evalate($file_id)
 	{
 		global $DB;
@@ -264,7 +264,7 @@ class User
 
 		$i=0;
 		while(i < count($D_list)) {
-			$query = $DB->MakeQuery("SELECT 자료id From 평가자료 where 개발자id=%s", $D_list[i++]);
+			$query = $DB->MakeQuery("SELECT 자료id From 평가자료 where 개발자id=%s", $D_list[$i++]);
 			$DFile_list[] = $DB->getColumn($query);
 
 			if(strpos($DFile_list,$file_id) == true) {
@@ -273,52 +273,6 @@ class User
 			else {
 				return false;
 			}
-		}
-	}
-
-	function File_Upload()
-	{
-		$target_dir = "uploads/";
-		$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
-		$uploadOk = 1;
-		$imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
-		// Check if image file is a actual image or fake image
-		if(isset($_POST["submit"])) {
-		    $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
-		    if($check !== false) {
-		        echo "File is an image - " . $check["mime"] . ".";
-		        $uploadOk = 1;
-		    } else {
-		        echo "File is not an image.";
-		        $uploadOk = 0;
-		    }
-		}
-		// Check if file already exists
-		if (file_exists($target_file)) {
-		    echo "Sorry, file already exists.";
-		    $uploadOk = 0;
-		}
-		// Check file size
-		if ($_FILES["fileToUpload"]["size"] > 500000) {
-		    echo "Sorry, your file is too large.";
-		    $uploadOk = 0;
-		}
-		// Allow certain file formats
-		if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
-		&& $imageFileType != "gif" ) {
-		    echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
-		    $uploadOk = 0;
-		}
-		// Check if $uploadOk is set to 0 by an error
-		if ($uploadOk == 0) {
-		    echo "Sorry, your file was not uploaded.";
-		// if everything is ok, try to upload file
-		} else {
-		    if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
-		        echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
-		    } else {
-		        echo "Sorry, there was an error uploading your file.";
-		    }
 		}
 	}
 
