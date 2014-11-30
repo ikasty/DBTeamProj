9c1886f966aa413e078471fd194d87170c489bcc
iKasty <ikasty@live.co.kr>
2014-11-30 18:32:47 +0900

    DB 구조 변경

--- dbproj.sql
+++ dbproj.sql
@@ -24,6 +24,26 @@ USE `dbproj`;
 -- --------------------------------------------------------
 
 --
+-- 테이블 구조 `유저`
+--
+
+CREATE TABLE IF NOT EXISTS `유저` (
+  `id` varchar(20) NOT NULL DEFAULT '' COMMENT '유저id (개발자id와 다름)',
+  `비밀번호` varchar(20) DEFAULT NULL COMMENT '비밀번호',
+  `이름` varchar(20) DEFAULT NULL COMMENT '이름',
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COMMENT='유저(관리자 + 개발자)의 개인 정보 관리';
+
+--
+-- 테이블의 덤프 데이터 `유저`
+--
+
+INSERT INTO `유저` (`id`, `비밀번호`, `이름`) VALUES
+('test', 'dGVzdA==', 'test id');
+
+-- --------------------------------------------------------
+
+--
 -- 테이블 구조 `개발자`
 --
 
@@ -34,11 +54,6 @@ CREATE TABLE IF NOT EXISTS `개발자` (
   `유저id` varchar(20) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COMMENT='개발자 신상정보';
-
---
--- 테이블의 덤프 데이터 `개발자`
---
-
 
 -- --------------------------------------------------------
 
@@ -54,11 +69,6 @@ CREATE TABLE IF NOT EXISTS `근무` (
   PRIMARY KEY (`회사이름`,`개발자id`,`입사일`,`퇴사일`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COMMENT='개발자의 근무 일지';
 
---
--- 테이블의 덤프 데이터 `근무`
---
-
-
 -- --------------------------------------------------------
 
 --
@@ -70,49 +80,6 @@ CREATE TABLE IF NOT EXISTS `부서` (
   `부서id` int(10) NOT NULL DEFAULT '0' COMMENT '부서id',
   PRIMARY KEY (`회사이름`,`부서id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COMMENT='회사 내의 부서들의 목록';
-
---
--- 테이블의 덤프 데이터 `부서`
---
-
-INSERT INTO `부서` (`회사이름`, `부서id`) VALUES
-('연세대학교', 0),
-('연세대학교', 1);
-
--- --------------------------------------------------------
-
---
--- 테이블 구조 `유저`
---
-
-CREATE TABLE IF NOT EXISTS `유저` (
-  `id` varchar(20) NOT NULL DEFAULT '' COMMENT '유저id (개발자id와 다름)',
-  `비밀번호` varchar(20) DEFAULT NULL COMMENT '비밀번호',
-  `이름` varchar(20) DEFAULT NULL COMMENT '이름',
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 COMMENT='유저(관리자 + 개발자)의 개인 정보 관리';
-
---
--- 테이블의 덤프 데이터 `유저`
---
-
-
--- --------------------------------------------------------
-
---
--- 테이블 구조 `자료분야`
---
-
-CREATE TABLE IF NOT EXISTS `자료분야` (
-  `자료id` varchar(20) NOT NULL DEFAULT '',
-  `자료분야` varchar(20) NOT NULL DEFAULT '',
-  PRIMARY KEY (`자료id`,`자료분야`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 COMMENT='자료의 자료분야';
-
---
--- 테이블의 덤프 데이터 `자료분야`
---
-
 
 -- --------------------------------------------------------
 
@@ -126,11 +93,6 @@ CREATE TABLE IF NOT EXISTS `전문분야` (
   PRIMARY KEY (`id`,`전문분야`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COMMENT='개발자의 전문 분야';
 
---
--- 테이블의 덤프 데이터 `전문분야`
---
-
-
 -- --------------------------------------------------------
 
 --
@@ -141,11 +103,6 @@ CREATE TABLE IF NOT EXISTS `평가` (
   `평가id` int(11) NOT NULL DEFAULT '0',
   PRIMARY KEY (`평가id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COMMENT='평가자 그룹이 실행한 평가의 id';
-
---
--- 테이블의 덤프 데이터 `평가`
---
-
 
 -- --------------------------------------------------------
 
@@ -161,11 +118,6 @@ CREATE TABLE IF NOT EXISTS `평가일정` (
   PRIMARY KEY (`평가회차`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COMMENT='평가의 일정 조정' AUTO_INCREMENT=1 ;
 
---
--- 테이블의 덤프 데이터 `평가일정`
---
-
-
 -- --------------------------------------------------------
 
 --
@@ -178,14 +130,6 @@ CREATE TABLE IF NOT EXISTS `평가자 그룹` (
   PRIMARY KEY (`평가회차id`,`그룹id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COMMENT='회차별 평가자 그룹의 목록';
 
---
--- 테이블의 덤프 데이터 `평가자 그룹`
---
-
-INSERT INTO `평가자 그룹` (`평가회차id`, `그룹id`) VALUES
-(0, 0),
-(0, 1);
-
 -- --------------------------------------------------------
 
 --
@@ -195,13 +139,9 @@ INSERT INTO `평가자 그룹` (`평가회차id`, `그룹id`) VALUES
 CREATE TABLE IF NOT EXISTS `평가자 선정` (
   `평가회차` int(11) NOT NULL DEFAULT '0' COMMENT '몇회차인지',
   `평가그룹` int(11) NOT NULL DEFAULT '0' COMMENT '어떤그룹인지',
-  `개발자id` varchar(20) NOT NULL COMMENT '어떤 개발자가'
+  `개발자id` varchar(20) NOT NULL COMMENT '어떤 개발자가',
+  PRIMARY KEY (`평가회차`, `개발자id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COMMENT='개발자의 어떤 평가 회차별 소속 평가자 그룹';
-
---
--- 테이블의 덤프 데이터 `평가자 선정`
---
-
 
 -- --------------------------------------------------------
 
@@ -217,10 +157,17 @@ CREATE TABLE IF NOT EXISTS `평가자료` (
   PRIMARY KEY (`자료id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COMMENT='평가자료 정보' AUTO_INCREMENT=1 ;
 
+-- --------------------------------------------------------
+
 --
--- 테이블의 덤프 데이터 `평가자료`
+-- 테이블 구조 `자료분야`
 --
 
+CREATE TABLE IF NOT EXISTS `자료분야` (
+  `자료id` varchar(20) NOT NULL DEFAULT '',
+  `자료분야` varchar(20) NOT NULL DEFAULT '',
+  PRIMARY KEY (`자료id`, `자료분야`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COMMENT='평가자료의 자료분야';
 
 -- --------------------------------------------------------
 
@@ -234,11 +181,6 @@ CREATE TABLE IF NOT EXISTS `평가지표` (
   `점수` int(11) DEFAULT NULL COMMENT '평가점수',
   PRIMARY KEY (`평가id`,`지표이름`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COMMENT='평가에 대한 지표와 그 평가점수';
-
---
--- 테이블의 덤프 데이터 `평가지표`
---
-
 
 -- --------------------------------------------------------
 
@@ -254,11 +196,6 @@ CREATE TABLE IF NOT EXISTS `평가하기` (
   PRIMARY KEY (`평가id`,`자료id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COMMENT='평가자 그룹이 실행한 평가의 결과 및 로그';
 
---
--- 테이블의 덤프 데이터 `평가하기`
---
-
-
 -- --------------------------------------------------------
 
 --
@@ -272,11 +209,6 @@ CREATE TABLE IF NOT EXISTS `피평가자 그룹` (
   PRIMARY KEY (`평가회차id`,`그룹id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COMMENT='피평가자 그룹이 어떤 회차에 어떤 평가자 그룹에게 평가받는지';
 
---
--- 테이블의 덤프 데이터 `피평가자 그룹`
---
-
-
 -- --------------------------------------------------------
 
 --
@@ -287,13 +219,8 @@ CREATE TABLE IF NOT EXISTS `피평가자 신청` (
   `평가회차` int(11) NOT NULL DEFAULT '0' COMMENT '어떤 회차에',
   `평가그룹` int(11) NOT NULL DEFAULT '0' COMMENT '어떤 그룹인지',
   `개발자id` varchar(20) NOT NULL DEFAULT '' COMMENT '피평가받는 개발자가',
-  PRIMARY KEY (`평가회차`,`평가그룹`,`개발자id`)
+  PRIMARY KEY (`평가회차`,`개발자id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COMMENT='피평가받는 개발자가 어떤 회차에 어떤 그룹인지';
-
---
--- 테이블의 덤프 데이터 `피평가자 신청`
---
-
 
 -- --------------------------------------------------------
 
