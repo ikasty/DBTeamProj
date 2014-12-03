420fc9e36ab789712fbf558011c775f7fc7a6262
iKasty <ikasty@live.co.kr>
2014-12-03 14:52:59 +0900

    func template 수정

--- func/func_template.php
+++ func/func_template.php
@@ -4,6 +4,10 @@ if (!defined("DBPROJ")) die(json_encode(-1));
 
 // ** add code here **
 
+// addMessage를 사용해서 알림영역에 메시지를 쓸 수 있음.
+// 주의: func에서만 사용 가능함
+addMessage("정상 작동합니다.");
+
 // return
 echo json_encode($AJAX);
 ?>
