bf8a60b25e1810781de1c5d8deb86a607c696482
iKasty <ikasty@live.co.kr>
2014-12-05 16:45:29 +0900

    회사 근무이력 추가

--- view/main/developer-graph.php
+++ view/main/developer-graph.php
@@ -2,7 +2,7 @@
 if (!defined("DBPROJ")) header('Location: /', TRUE, 303);
 
 ///////////////////////////////
-$dummy_data = array(
+$dummy_stat_data = array(
 	1 => array(
 		'count' => 1,
 		'date-start' => "2014-12-01",
@@ -29,13 +29,32 @@ $dummy_data = array(
 		)
 	)
 );
+$dummy_work_data = array(
+	array(
+		'date-start' => "2014-03-01",
+		'date-end' => "",
+		'company' => "연세대",
+		"dept" => "컴퓨터과학과"),
+	array(
+		'date-start' => "2008-03-02",
+		'date-end' => "2011-03-02",
+		'company' => "망한회사",
+		'dept' => "망한부서"),
+	array(
+		'date-start' => "2003-06-15",
+		'date-end' => "2007-12-30",
+		'company' => "프리랜서",
+		'dept' => "")
+);
 ///////////////////////////////
 
-$data = $dummy_data; // 실제 데이터 불러오는 코드 넣을 것
+///////////////////////////////
+// stat data
+$data = $dummy_stat_data; // 실제 데이터 불러오는 코드 넣을 것
 $label = array_keys($data);
 array_walk($label, create_function('&$value', ' $value .= "회차"; ')); // 1회차, 2회차, 3회차...
 
-$result = array("labels" => $label, "datasets" => array(
+$stat_all = array("labels" => $label, "datasets" => array(
 	array(
 		"label" => "평균 데이터",
 		"fillColor" => "rgba(151,187,205,0.5)",
@@ -50,33 +69,64 @@ foreach ($data as $count => $value) {
 	foreach($value["evaluate"] as $dataset) {
 		//dataset의 speed, size, etc.. 등을 고려하여 합과 개수를 구함
 	}
-	$result["datasets"][0]["data"][] = ($count != 0) ? ($sum / $count) : 0;
+	$stat_all["datasets"][0]["data"][] = ($count != 0) ? ($sum / $count) : 0;
+}
+
+///////////////////////////////
+// work data
+$work_data = $dummy_work_data; // 실제 데이터 불러오는 코드 넣을 것
+foreach ($work_data as &$value) {
+	$value['date-start'] = str_replace('-', '/', substr($value['date-start'], 0, 7));
+	if ($value['date-end'] !== '')
+		$value['date-end'] = str_replace('-', '/', substr($value['date-end'], 0, 7));
 }
 
 ?>
 <div class="pure-g graph-area">
-	<div class="pure-u-2-3">
+	<div class="pure-u-3-5">
 		<div class="mainform">
 			<div class="box-title" style="background: #519251;">
 				<span class="mega-octicon octicon-graph"></span> 내 평가 통계
 			</div>
-			<div style="margin:10px; display:inline-block;">
+			<div style="margin:15px; display:inline-block;">
 				<p>최근 6회간 평균 평가점수</p>
-				<canvas id="eval-stat-all" width="700" height="200"></canvas>
+				<canvas id="eval-stat-all" width="500" height="200"></canvas>
 			</div>
 		</div>
 	</div>
-	<div class="pure-u-1-3">
+	<div class="pure-u-2-5">
 		<div class="mainform">
 			<div class="box-title" style="background: #519251;">
-				<span class="mega-octicon octicon-briefcase"></span> 근무 이력
-			</div>	
+				<span class="mega-octicon octicon-briefcase"></span> 내 근무 이력
+			</div>
+			<div style="padding:20px;">
+				<table class="pure-table pure-table-horizontal" style="margin:0 auto;">
+				<thead>
+					<tr>
+						<th>회사명</th>
+						<th>부서명</th>
+						<th>근무기간</th>
+					</tr>
+				</thead>
+				<tbody>
+				<? $count = 1; ?>
+				<? foreach ($work_data as $company) : ?>
+				<? $class = (($count++) % 2 == 0 ? ' class="pure-table-odd"' : "" ) ?>
+					<tr<?=$class?>>
+						<td><?=$company['company'];?></td>
+						<td><?=$company['dept'];?></td>
+						<td><?=$company['date-start'];?> ~ <?=($company['date-end'] === "") ? ("현재") : ($company['date-end'])?></td>
+					</tr>
+				<? endforeach; ?>
+				</tbody>
+				</table>
+			</div>
 		</div>
 	</div>
 	<script type="text/javascript">
 		$(document).ready(function(){
 			var eval_stat_all_ctx = $("#eval-stat-all").get(0).getContext("2d");
-			var eval_stat_all_data = <?=json_encode($result)?>;
+			var eval_stat_all_data = <?=json_encode($stat_all)?>;
 			var eval_stat_all_chart = new Chart(eval_stat_all_ctx).Bar(eval_stat_all_data);
 		});
 	</script>
