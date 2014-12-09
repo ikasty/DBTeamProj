3fe51c2ac1d0dd2516dcc81b34db9f0f2bd2fe2a
iKasty <ikasty@live.co.kr>
2014-12-10 01:16:35 +0900

    search func 추가

---
+++ func/search/company.php
@@ -0,0 +1,39 @@
+<?
+header('Content-Type: application/json');
+if (!defined("DBPROJ")) die(json_encode(-1));
+
+if (sizeof($ARGS["view"]) == 0) die(json_encode(-1));
+$db = getDB();
+
+// query문
+$query = "SELECT ";
+$query .= implode(',', $ARGS["view"]);
+if ($ARGS["company-name"] !== "" || $ARGS["company-major"] !== "") {
+	$query .= " WHERE ";
+}
+$query .= " ORDER BY " $ARGS["sort-type"];
+if ($ARGS["asc-desc"] === 'true')
+	$query .= " DESC";
+
+$result = $db->getResult($query);
+
+// 변수는 $ARGS에 담겨있음
+var_dump($ARGS);
+
+$return["success"] = "success";
+ob_start();
+?>
+<table class="pure-table pure-table-horizontal">
+<thead>
+	<tr>
+<? foreach ($ARGS["view"] as $name) : ?>
+		<th><?=$name?></th>
+<? endforeach; ?>
+	</tr>
+</thead>
+<tbody>
+</tbody>
+</table>
+<?
+$return["html"] = ob_end_clean();
+?>
--- view/search/company.php
+++ view/search/company.php
@@ -15,7 +15,7 @@ if (!defined("DBPROJ")) header('Location: /', TRUE, 303);
 		
 			<label>검색 결과 선택</label>
 			<label for="company-name-view" class="pure-checkbox">
-				<input id="company-name-view" class="view-array" type="checkbox" value="이름" checked>
+				<input id="company-name-view" class="view-array" type="checkbox" value="회사이름" checked>
 				회사이름 보이기
 			</label>
 			<label for="company-major-view" class="pure-checkbox">
@@ -25,6 +25,17 @@ if (!defined("DBPROJ")) header('Location: /', TRUE, 303);
 			<label for="company-eval-view" class="pure-checkbox">
 				<input id="company-eval-view" class="view-array" type="checkbox" value="회사평가" checked>
 				회사평가 보이기
+			</label>
+
+			<label>정렬 방식</label>
+			<select id="sort-type">
+				<option>회사이름</option>
+				<option>전문분야</option>
+				<option>회사평가</option>
+			</select>
+			<label for="asc-desc" class="pure-checkbox">
+				<input id="company-asc-desc" type="checkbox">
+				내림차순 정렬
 			</label>
 
 			<a id="search-btn" class="ajax_load pure-button pure-button-primary" data-func="search/company">검색하기</a>
@@ -41,7 +52,9 @@ if (!defined("DBPROJ")) header('Location: /', TRUE, 303);
 		args["company-major"] = $("#company-major").val();
 		args["view"] = [];
 			// args["view"] 에 체크된 체크박스 value 넣기
-			$(".data-chkbox:checked").each(function(){args["view"].push($(this).val())})
+			$(".view-array:checked").each(function(){args["view"].push($(this).val())})
+		args["sort-type"] = $("#sort-type").val();
+		args["asc-desc"] = $("#company-asc-desc").is(":checked");
 		return true;
 	})
 	.on('finish', function (event, item, data) {
